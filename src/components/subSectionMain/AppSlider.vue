804611e5b99fe85946790389e713ec85a80346d0
DarioDC95 <dario_95@live.com>
2023-01-25 13:24:20 +0100

    CardSlider and mock-up Done. Now I'm trying to implement the slider

--- src/components/subSectionMain/AppSlider.vue
+++ src/components/subSectionMain/AppSlider.vue
@@ -1,19 +1,119 @@
 <script>
+    import AppCardSlider from '../sunCardComponents/AppCardSlider.vue';
     export default {
-
+        components: {
+            AppCardSlider,
+        },
+        data() {
+            return {
+                cardsSlider: [
+                    {
+                        image: '/src/assets/8wa60okr-1-790x576.jpg',
+                        title: 'Basket of Flower on table',
+                        area: 'Branding Strategy'
+                    },
+                    {
+                        image: '/src/assets/84316050-0af0-49db-a53a-241d47ddad0e-2-790x576.jpg',
+                        title: 'Purinky Products',
+                        area: 'Digital Experience'
+                    },
+                    {
+                        image: '/src/assets/a247b00b-3621-470f-b4b8-b3ac46f25907-1-790x576.jpg',
+                        title: 'Satisfy Poster',
+                        area: 'Branding Strategy'
+                    },
+                    {
+                        image: '/src/assets/84316050-0af0-49db-a53a-241d47ddad0e-2-790x576.jpg',
+                        title: 'Mock-up Template',
+                        area: 'Ecommerce'
+                    },
+                    {
+                        image: '/src/assets/a247b00b-3621-470f-b4b8-b3ac46f25907-1-790x576.jpg',
+                        title: 'Landing Page',
+                        area: 'Digital Strategy'
+                    },
+                ]
+            }
+        }
     }
 </script>
 
 <template>
     <section class="slider">
-        Qui andrà lo slider.
+        <div class="mycontainer">
+            <div class="title-row d-flex justify-content-between flex-nowrap">
+                <div class="mycol-6">
+                    <div class="mycard">
+                        <div class="top-title fw-semibold">Portfolio</div>
+                        <div class="title mb-5"><span class="fw-bold">latest</span> work</div>
+                    </div>
+                </div>
+                <div class="mycol-6">
+                    <div class="mycard d-flex align-items-end">
+                        <button class="first-button"><i class="fa-solid fa-arrow-left"></i></button>
+                        <button><i class="fa-solid fa-arrow-right"></i></button>
+                    </div>
+                </div>
+            </div>
+            <div class="content-row d-flex">
+                <div v-for="(value, index) in cardsSlider" :key="index" :id="`col_Slider-${index}`" class="mycol">
+                    <AppCardSlider :element="value"/>
+                </div>
+            </div>
+        </div>        
     </section>
 </template>
 
 <style scoped lang="scss">
+    @use '../../styles/partials/variables.scss'as *;
+    @use '../../styles/partials/mixins.scss'as *;
 
     .slider {
         padding: 100px 0;
-        text-align: center;
+        overflow: hidden;
+        background-color: #f6f6f6;
+
+        .title-row {
+            margin-bottom: 40px;
+    
+            .top-title {
+                color: $text-red;
+                font-size: $fs-sm;
+            }
+            
+            .title {
+                color: $text-black;
+                font-size: $fs-xl;
+            }
+
+            .first-button {
+                margin-right: 30px;
+            }
+
+            button {
+                width: 50px;
+                height: 50px;
+                border: 1px solid $text-orange;
+                border-radius: 50%;
+                background-color: $bg-white;
+                color: $text-orange;
+                font-size: 25px;
+                transform: translateY(50%);
+
+                &:hover {
+                    box-shadow: 0px 0px 30px 0px rgba(128, 128, 128, 0.884);
+                    background: $bg-linear-orange;
+                    color: $text-white;
+                }
+            }
+        }
+
+        .content-row {
+            position: relative;
+
+            .mycol {
+                margin-right: 50px;
+            }
+        }
     }
 </style>
---
+++ src/components/sunCardComponents/AppCardSlider.vue
@@ -0,0 +1,79 @@
+<script>
+    export default {
+        props: {
+            element: Object,
+        }
+    }
+</script>
+
+<template>
+    <div class="mycard">
+        <div class="img">
+            <img :src="element.image" alt="Image">
+        </div>
+        <div class="description d-flex py-4 px-5 mt-1 justify-content-between align-items-baseline">
+            <h5 class="fw-bold">{{element.title}}</h5>
+            <div class="label">{{element.area}}</div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    @use '../../styles/partials/variables.scss'as *;
+    @use '../../styles/partials/mixins.scss'as *;
+
+    .mycard {
+        width: 500px;
+        border-radius: 40px;
+        position: relative;
+        background-color: white;
+        pointer-events: all;
+        z-index: 100;
+
+        &::before {
+            opacity: 0;
+            content: '';
+            display: block;
+            position: absolute;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            z-index: 50;
+            border-radius: 40px;
+            background: $bg-linear-orange-180dg;
+            transition: opacity 0.8s;
+        }
+
+        &:hover {
+
+            &::before {
+                opacity: 1;
+            }
+
+            h5 {
+                color: $text-white;
+            }
+        }
+
+        .img {
+            border-radius: 40px;
+            overflow: hidden;
+            position: relative;
+            z-index: 100;
+
+            img {
+                max-width: 100%;
+            }
+        }
+
+        .description {
+            position: relative;
+            z-index: 100;
+
+            .label {
+                color: $text-grey;
+                font-size: $fs-sm;
+            }
+        }
+    }
+</style>
